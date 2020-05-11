4ac5a8a9973c838c45982a64e484455cfe702c4f
Marko <marko_poikkimaki@hotmail.com>
2020-05-11 14:14:12 +0200

    random number

--- hello.go
+++ hello.go
@@ -1,13 +1,21 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"math/rand")
 
 func main() {
 
 	for i := 0; i < 10; i++ {
 		helloWorld(i)
+		randomNumber(i)
 	}
 
+}
+
+func randomNumber(i int) {
+	r := rand.Intn(100)
+	sum := i * r
+	fmt.Printf("Random number: %d : %d : %d \n", i, r, sum)
 }
 
 func helloWorld(i int) {
